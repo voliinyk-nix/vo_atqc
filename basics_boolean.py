df1169d3eca35986ff30ca127e0edcef56366b3b
voliinyk-nix <voliinyk@n-ix.com>
2020-10-06 20:06:50 +0300

    basic_variables.py

---
+++ basics_boolean.py
@@ -0,0 +1,9 @@
+# use conditions "if" and  "case"
+# Create 4 boolean variables(true,true,false,false) and compare them between themselves - result print in console.
+
+boolean_true_1 = true
+boolean_true_2 = true
+boolean_false_1 = true
+boolean_false_2 = true
+
+print(boolean_true_1 == boolean_true_2)
