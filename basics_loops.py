8c5176d6fe99b8e1568ea4d27584a9c48d0a3c6f
voliinyk-nix <voliinyk@n-ix.com>
2020-10-12 19:59:20 +0300

    basics_loops.py

---
+++ basics_loops.py
@@ -0,0 +1,6 @@
+# use cycles "for",  "while",  "foreach"Cycle operators
+# Create for, while, do while cycles that iterate 10 times and print iteration number to console.
+# Create recursion with exit condition.
+# Create an infinite loop.
+# Create recursion without exit condition.
+
