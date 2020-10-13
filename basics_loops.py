41b68e089504a4da19a4f7e3d9ee713f7645a86e
voliinyk-nix <voliinyk@n-ix.com>
2020-10-13 19:44:04 +0300

    basics_loops upd

--- basics_loops.py
+++ basics_loops.py
@@ -1,6 +1,32 @@
 # use cycles "for",  "while",  "foreach"Cycle operators
 # Create for, while, do while cycles that iterate 10 times and print iteration number to console.
-# Create recursion with exit condition.
-# Create an infinite loop.
-# Create recursion without exit condition.
 
+for x in range(10):
+    print(x)
+
+x = 0
+while x < 10:
+    print(x)
+    x = x + 1
+
+# Create an infinite loop.
+inf_loop = 0
+while inf_loop < 1:
+    print("One more row from infinite loop")
+    break
+
+
+# Create recursion with exit condition.
+def print_row():
+    print("Print the row")
+
+print_row()
+print("Exit")
+
+
+# Create recursion without exit condition.
+def no_exit():
+    print("No exit condition")
+    no_exit()
+
+no_exit()
